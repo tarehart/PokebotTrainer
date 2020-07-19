ba3618b82f612dc7f6bb8f39bc86a169d4222662
Tyler Arehart <tarehart@gmail.com>
2020-07-18 18:44:20 -0700

    Improved spawn management, leveraging new framework changes.

--- pokebot_trainer.py
+++ pokebot_trainer.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
+from random import randint
 from threading import Thread
 from time import sleep
 from typing import List, Dict
 from pathlib import Path
 
 from rlbot.agents.base_script import BaseScript
-from rlbot.matchconfig.match_config import MatchConfig, PlayerConfig, MutatorConfig
+from rlbot.matchconfig.match_config import MatchConfig, PlayerConfig, MutatorConfig, EmptyPlayerSlot
 from rlbot.parsing.bot_config_bundle import BotConfigBundle
 from rlbot.parsing.directory_scanner import scan_directory_for_bot_configs
 from rlbot.setup_manager import SetupManager
@@ -19,7 +20,7 @@ from urllib3.exceptions import MaxRetryError
 
 SPAWN = 'spawn'
 BOT_DIRECTORY = Path(__file__).parent / 'bots'
-LIFESPAN = 90
+LIFESPAN = 15
 
 class MyActionBroker(BotActionBroker):
     def __init__(self, script):
@@ -37,14 +38,23 @@ class MyActionBroker(BotActionBroker):
 class ActiveBot:
     name: str
     team: int
+    spawn_id: int
     join_time: float
     bundle:  BotConfigBundle
+
 
 def highlight_team_color(text: str, team: int) -> str:
     team_class = 'blue' if team == 0 else 'orange'
     return f'<span class="player-name {team_class}">{text}</span>'
 
-def create_player_config(name: str, team: int, config_path: str):
+
+def player_config_from_active_bot(active_bot: ActiveBot):
+    if active_bot is None:
+        return EmptyPlayerSlot()
+    return create_player_config(active_bot.name, active_bot.team, active_bot.spawn_id, active_bot.bundle.config_path)
+
+
+def create_player_config(name: str, team: int, spawn_id: int, config_path: str):
     player_config = PlayerConfig()
     player_config.bot = True
     player_config.rlbot_controlled = True
@@ -52,6 +62,7 @@ def create_player_config(name: str, team: int, config_path: str):
     player_config.human_index = 0
     player_config.name = name
     player_config.team = team
+    player_config.spawn_id = spawn_id
     player_config.config_path = config_path
     return player_config
 
@@ -64,6 +75,8 @@ class PokebotTrainer(BaseScript):
         self.known_players: List[PlayerInfo] = []
         self.active_bots: List[ActiveBot] = []
         self.available_bots: Dict[str, BotConfigBundle] = {b.name: b for b in self.get_bots()}
+        self.available_bot_names: List[str] = list(self.available_bots.keys())
+        self.available_bot_names.sort()
         self.logger.info(f"Pokebot trainer has: {self.available_bots.keys()}")
         self.setup_manager = SetupManager()
 
@@ -84,12 +97,12 @@ class PokebotTrainer(BaseScript):
 
     def get_actions_currently_available(self) -> List[AvailableActions]:
         actions = []
-        for name in self.available_bots.keys():
+        for name in self.available_bot_names:
             blue_text = highlight_team_color(f"blue {name}", 0)
             actions.append(BotAction(description=f"Spawn {blue_text}", action_type=SPAWN, data={'name': name, 'team': 0}))
+        for name in self.available_bot_names:
             orange_text = highlight_team_color(f"orange {name}", 1)
             actions.append(BotAction(description=f"Spawn {orange_text}", action_type=SPAWN, data={'name': name, 'team': 1}))
-        print("Fetching pokeactions")
         return [AvailableActions(self.name, None, actions)]
 
     def process_choice(self, choice: BotAction):
@@ -99,18 +112,27 @@ class PokebotTrainer(BaseScript):
             team = choice.data['team']
             bundle = self.available_bots[name]
 
-            self.active_bots.append(ActiveBot(name, team, self.game_tick_packet.game_info.seconds_elapsed, bundle))
+            names = set([ab.name for ab in self.active_bots if ab is not None])
+            unique_name = name[:31]
+            count = 2
+            while unique_name in names:
+                unique_name = f'{name[:27]} ({count})'  # Truncate at 27 because we can have up to '(10)' appended
+                count += 1
+
+            self.active_bots.append(ActiveBot(unique_name, team, randint(1, 65535), self.game_tick_packet.game_info.seconds_elapsed, bundle))
             self.relaunch_bots()
 
     def relaunch_bots(self):
         match_config = MatchConfig()
-        match_config.player_configs = [create_player_config(ab.name, ab.team, ab.bundle.config_path) for ab in self.active_bots]
+        match_config.player_configs = [player_config_from_active_bot(ab) for ab in self.active_bots]
         match_config.game_mode = 'Soccer'
         match_config.game_map = 'DFHStadium'
         match_config.existing_match_behavior = 'Continue And Spawn'
         match_config.mutators = MutatorConfig()
 
         self.setup_manager.load_match_config(match_config)
+        self.setup_manager.launch_early_start_bot_processes()
+        self.setup_manager.try_recieve_agent_metadata()
         self.setup_manager.start_match()
         self.setup_manager.launch_bot_processes()
 
@@ -127,9 +149,20 @@ class PokebotTrainer(BaseScript):
             self.get_game_tick_packet()
             self.setup_manager.try_recieve_agent_metadata()
             game_seconds = self.game_tick_packet.game_info.seconds_elapsed
-            fresh_bots = [b for b in self.active_bots if game_seconds < b.join_time + LIFESPAN]
-            if len(fresh_bots) < len(self.active_bots):
-                self.active_bots = fresh_bots
+
+            needs_relaunch = False
+            next_bots = []
+            for b in self.active_bots:
+                if b is not None and game_seconds > b.join_time + LIFESPAN:
+                    next_bots.append(None)
+                    needs_relaunch = True
+                else:
+                    next_bots.append(b)
+
+            if needs_relaunch:
+                while len(next_bots) > 0 and next_bots[-1] is None:
+                    next_bots.pop()  # Get rid of any trailing None values.
+                self.active_bots = next_bots
                 self.relaunch_bots()
 
             sleep(0.1)
